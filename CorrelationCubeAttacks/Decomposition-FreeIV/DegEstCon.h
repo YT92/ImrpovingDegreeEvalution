537bb71111d715ce7cf6c39fe8352f52bc0b72f2
YT92 <52812495+YT92@users.noreply.github.com>
2020-08-30 22:43:59 +0800

    Add files via upload

---
+++ CorrelationCubeAttacks/Decomposition-FreeIV/DegEstCon.h
@@ -0,0 +1,16 @@
+int DEGLA(int Da[], int Db[], int Dc[], int t, int dim);
+int DEGLB(int Da[], int Db[], int Dc[], int t, int dim);
+int DEGLC(int Da[], int Db[], int Dc[], int t, int dim);
+int DegMulA(int Da[], int Db[], int Dc[], int t, int dim);
+int DegMulB(int Da[], int Db[], int Dc[], int t, int dim);
+int DegMulC(int Da[], int Db[], int Dc[], int t, int dim);
+int DegEst(int Da[], int Db[], int Dc[], int round, int N0, int dim);
+int DegEstWithCon(int cube[], int dim, int round, int N0);
+int DegEstWithConV(int cube[], int dim, int round, int N0);
+void getX(int cube[], int loclist[], int X[], int dim, int loclen);
+int DegEstVerify(int cube[], int dim, int round, int N0, int pos, int posr);
+void GetandTestX();
+void GetandTestXVerify();
+void GetandTestXFreeIV();
+int DegEstFreeNCV2(int cube[], int dim, int round, int N0, int pos[], int posr[],int posnum,int freenc);
+void GetandTestXFreeIVV2();
---
+++ CorrelationCubeAttacks/Decomposition-FreeIV/main.cpp
@@ -0,0 +1,23 @@
+//实现Decomposition算法
+#include<stdio.h>
+#include<Windows.h>
+#include"DegEstCon.h"
+int main()
+{
+	FILE *fp;
+	//fp = fopen("rec.txt", "r");
+	char filename[40] = "rec.txt";
+	fopen_s(&fp, filename, "a+");
+	//int cube[80] = { 2,4,6,8,10,12,14,17,19,21,23,25,27,29,32,34,36,38,40,42,44,47,49,51,53,57,62,79};
+	//int cube[80] = { 0,2,4,5,6,7,9,11,13,14,15,18,20,22,24,26,32,35,37,39,42,44,46,48,53,55,57,61,68,70,72,79 };
+	int cube[80] = { 12,63,78,40,56,49,36,7,11,38,0,26,57,71,55,74,22,58,37,62,50,29 };
+	int i;
+	int dim, roundnum,flag;
+	int N0 = 200;
+	dim = 22;
+	roundnum = 704;
+	//有一个循环
+	GetandTestXFreeIVV2();
+	system("pause");
+	return 0;
+}
